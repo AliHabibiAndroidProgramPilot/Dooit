b5aa7867cc40ba5550ca1749fc2836a318d4bb3c
Ali <biz.alihabibi@gmail.com>
2025-03-16 21:43:09 +0330

    Pinned List Recycler

--- app/src/main/java/com/ali/dooit/db/Converters.kt
+++ app/src/main/java/com/ali/dooit/db/Converters.kt
@@ -5,11 +5,13 @@ import java.time.Instant
 
 class Converters {
 
+    // For Code Usage
     @TypeConverter
     fun timeStampToInstance(value: Long?): Instant? {
         return value?.let { Instant.ofEpochMilli(it) }
     }
 
+    // For Database Saving
     @TypeConverter
     fun instanceToTimeStamp(instant: Instant?): Long? {
         return instant?.toEpochMilli()
