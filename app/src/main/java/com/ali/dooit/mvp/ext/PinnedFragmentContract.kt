e8440ab572918197d4808167992fa08fa7d7cbfe
Ali <biz.alihabibi@gmail.com>
2025-03-26 19:17:50 +0330

    Base Design Of Add Task Activity

--- app/src/main/java/com/ali/dooit/mvp/ext/PinnedFragmentContract.kt
+++ app/src/main/java/com/ali/dooit/mvp/ext/PinnedFragmentContract.kt
@@ -5,27 +5,19 @@ import com.ali.dooit.db.entities.relations.TaskWithTaskSubItems
 interface PinnedFragmentContract {
 
     interface View {
-
         fun initPinnedRecycler(pinnedTasks: ArrayList<TaskWithTaskSubItems>)
-
     }
 
     interface Presenter {
-
         fun attachView(view: View)
-
         fun viewCaller() {}
-
         fun detachView()
 
     }
 
     interface Model {
-
         suspend fun getTasksByState(isPinned: Boolean): ArrayList<TaskWithTaskSubItems>
-
         fun closeDatabase()
-
     }
 
 }
